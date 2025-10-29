db16002752681872e5f92e96050a2c7ae9d6e611
rizky <rizky@acerubuntuserver.tailea2d68.ts.net>
2025-10-29 09:46:39 +0000

    Wed Oct 29 09:46:39 AM UTC 2025

--- public-backup.sh
+++ public-backup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Ensure branch is up to date
+git fetch origin
+
+#Pull remote branch, rearrange local edits on top
+git pull --rebase
+
 # Add all files to queue
 git add .
 
@@ -7,9 +13,9 @@ git add .
 message=`date`
 
 # Add a new commit with today's date and time
-echo "Creating a new commit"
+echo "Creating a new commit: backup: $(date)"
 git commit -m "$message"
 
 #Push to main
 echo "Pushing the files to repo"
-git push origin
+git push --force-with-lease origin main
