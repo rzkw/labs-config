75fbbc98cb51c9702bdb2b19e34b51efac30bae8
Rizky <167944030+rzkw@users.noreply.github.com>
2026-01-12 14:22:14 +0700

    removed --dry-run, changed date/time format, added log file

--- rsync-backup/rsync-backup.sh
+++ rsync-backup/rsync-backup.sh
@@ -6,13 +6,13 @@ time=$(date +%T)
 backupdir="/media/backup-acer"
 origin="/etc/alloy   /etc/containerd   /etc/docker   /etc/fail2ban   /etc/initramfs-tools   /etc/nginx   /etc/selinux   /etc/ssh   /etc/sudoers   /etc/sudoers.d   /etc/ufw   /etc/passwd   /etc/group   /etc/shadow   /etc/gshadow   /etc/crontab   /etc/cron.d   /etc/cron.daily   /etc/cron.hourly   /etc/cron.weekly   /etc/cron.monthly"
 
-mkdir $backupdir/$date
+mkdir $backupdir/$date-$time
 
 echo "Backing up /etc core directories to /media/backup-acer/etc-core"
 
 # Backup - create archive; recursive into dirs; verbose; update only files that are older; compress files during transfer
 
-rsync -arvuz --dry-run -delete -progress -stats $origin "$backupdir"/"$date" \
+rsync -arvuz -delete -progress -stats $origin "$backupdir"/"$date-$time" \
 2>&1 | tee $backupdir/backup-log-$date-$time.txt
 
 echo "Backup complete"
